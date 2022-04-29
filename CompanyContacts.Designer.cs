9f6e0ae32bcd993ad81aea68d9b2faf17607be00
louismon <mondesir_louis@yahoo.fr>
2022-04-29 19:45:43 -0400

    Changed to my account for testing

--- CompanyContacts.Designer.cs
+++ CompanyContacts.Designer.cs
@@ -63,6 +63,7 @@
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(784, 318);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label2
             // 
--- CompanyContacts.cs
+++ CompanyContacts.cs
@@ -72,5 +72,10 @@ namespace DBFinal
             back.Show();
 
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
