7ca180c5842b4ac430836f031312f03c5060b3fe
kaidenpage <88171816+kaidenpage@users.noreply.github.com>
2022-04-29 15:41:11 -0400

    Supplier/Vendor Delete

--- Supplier.Designer.cs
+++ Supplier.Designer.cs
@@ -63,6 +63,7 @@
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(784, 318);
             this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // button1
             // 
--- Supplier.cs
+++ Supplier.cs
@@ -71,5 +71,92 @@ namespace DBFinal
             Employee back = new Employee("User");
             back.Show();
         }
+
+        public void BindGrid()
+        {
+
+            const string HOST = "localhost:5432";
+            const string USER = "postgres";
+            const string PASS = "123411";
+            const string DB = "postgres";
+            const string connectionString = $"Host={HOST};Username={USER};Password={PASS};Database={DB}";
+            var conn = new NpgsqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+                string sql = @"select * from supplier";
+                NpgsqlCommand cmd = new NpgsqlCommand();
+
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+                    dataGridView1.DataSource = dt;
+                }
+
+
+
+                conn.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Something went wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close();
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(MessageBox.Show("Are you sure you want to delete?", "Delete record",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Console.WriteLine("Deleting vendors attempt");
+                //var qmanager = new QueryManager();
+                //qmanager.Clientlogin(textBox1.Text, textBox2.Text);
+
+                const string HOST = "localhost:5432";
+                const string USER = "postgres";
+                const string PASS = "123411";
+                const string DB = "postgres";
+                const string connectionString = $"Host={HOST};Username={USER};Password={PASS};Database={DB}";
+                var conn = new NpgsqlConnection(connectionString);
+
+
+                try
+                {
+                    conn.Open();
+                    string name = dataGridView1.Rows[e.RowIndex].Cells["name"].FormattedValue.ToString();
+                    //string sql = @"select * from supplier";
+                    NpgsqlCommand cmd = new NpgsqlCommand("Delete supplier where name = '" + name + "' ", conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Successfully Deleted");
+
+
+
+
+
+
+                    conn.Close();
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Something went wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    conn.Close();
+                }
+                BindGrid();
+            }
+           
+        }
     }
 }
